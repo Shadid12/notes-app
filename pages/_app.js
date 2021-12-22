01d9a20f014c4e04e560719c1f6f51f14f40000b
shaque <shaque@omers.com>
2021-12-22 09:07:04 -0500

    added markdown files

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
 import '../styles/globals.css'
+// import 'highlight.js/styles/default.css';
+
+
+import 'github-markdown-css'
+import 'highlight.js/styles/vs.css'
+import 'katex/dist/katex.css'
+import 'bytemd/dist/index.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
